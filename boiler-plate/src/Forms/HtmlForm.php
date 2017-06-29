9e53616ab276656d72eb5fb6e114a5ec430f966d
Khalifah K. Shabazz <shabazzk@gmail.com>
2017-06-28 22:49:44 -0400

    Initial commit.

---
+++ boiler-plate/src/Forms/HtmlForm.php
@@ -0,0 +1,42 @@
+<?php namespace {{ namespace }}\Forms;
+
+use Kshabazz\Slib\Tools\Utilities;
+use Whip\Form;
+use Whip\Lash\Validator;
+
+/**
+ * Class HtmlForm
+ *
+ * Common form abilities.
+ *
+ * @package Suh\Forms
+ */
+abstract class HtmlForm implements Form
+{
+    use Utilities;
+
+    /** @var array Client form input. */
+    protected $input;
+
+    /** @var \Whip\Lash\Validation */
+    protected $validation;
+
+    /**
+     * Login constructor.
+     *
+     * @param \Whip\Lash\Validator $validation
+     */
+    public function __construct(Validator $validation)
+    {
+        $this->validation = $validation;
+        $this->input = [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getRenderData() : array
+    {
+        return [ $this->input, 'errors' => $this->validation->getErrors()];
+    }
+}
